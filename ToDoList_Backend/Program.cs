0cec4d2c8b21afe2c8b9c74ecfa77718dbde3080
szymon adamowicz <adamowiczszymon8@gmail.com>
2025-03-21 23:55:29 +0100

    commit fix, didnt include removed files

--- ToDoList_Backend/Program.cs
+++ ToDoList_Backend/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<Task_data>();
+builder.Services.AddSingleton<TaskData>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
