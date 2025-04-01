c63e5f90645bf1af8061640476f21f53a6237675
szymon adamowicz <adamowiczszymon8@gmail.com>
2025-04-01 23:08:56 +0200

    weird bug with language

--- ToDoList_Backend/Program.cs
+++ ToDoList_Backend/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
             .AllowAnyHeader()
             .WithOrigins(
                 "http://localhost:3000",
-                       "https://todolistfrontend-31yu53fq8-szymons-projects-1e0e4c1e.vercel.app"
+                       "https://todolistfrontend-dsnwucuma-szymons-projects-1e0e4c1e.vercel.app"
             )
     );
 });
